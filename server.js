e19830d079c73afde664740f05ca89c7dfbd620e
robert.schmidt04@telekom.de <robert.schmidt04@telekom.de>
2026-03-02 15:35:43 +0100

    chore: update express to version 5.0.0 and modify route handling in server.js

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ app.get('/health', (req, res) => {
 });
 
 // Serve the main index.html for SPA
-app.get('*', (req, res) => {
+app.get('*path', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
